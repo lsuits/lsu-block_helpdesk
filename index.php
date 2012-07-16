4ecd65745bb925733519ef584c3fd67ac8d260ac
Philip Cali <philip.cali@gmail.com>
2012-07-16 08:46:20 -0500

    Remove smarty on index

--- index.php
+++ index.php
@@ -2,15 +2,13 @@
 
 require_once('../../config.php');
 require_once('lib.php');
-require_once($CFG->libdir. '/quick_template/lib.php');
+require_once('searchform.php');
 
 require_login();
 
 $mode = optional_param('mode', 'course', PARAM_ALPHA);
 
 $blockname = get_string('pluginname', 'block_helpdesk');
-
-$data = data_submitted();
 
 if ($mode == 'user') {
     $header = get_string('search_users', 'block_helpdesk');
@@ -22,28 +20,33 @@ if ($mode == 'user') {
     );
     $fields = 'id, firstname, lastname, idnumber';
 
-    $sql = $data ? hdesk_get_results_sql($data, $criterion) : null;
-    $results = $sql ? $DB->get_records_select($mode, $sql, null, '', $fields) : array();
+    $results = function($data, &$count) use ($criterion, $mode, $fields, $DB) {
+        $sql = $data ? hdesk_get_results_sql($data, $criterion) : null;
+        $count = $sql ? $DB->count_records_select($mode, $sql) : 0;
+        return $sql ? $DB->get_records_select($mode, $sql, null, '', $fields) : array();
+    };
+
     $follow_link = new moodle_url('/user/view.php');
 } else {
     $header = get_string('search_courses', 'block_helpdesk');
     $criterion = array('fullname' => get_string('fullname'));
-    $results = $data ?
-        get_courses_search(explode(' ', $data->fullname_terms), 'fullname ASC', 0, 50, $count) :
-        array();
+    $results = function($data, &$count) {
+        $fullnames = explode(' ', $data->fullname_terms);
+        return $data ?
+            get_courses_search($fullnames, 'fullname ASC', 0, 50, $count) :
+            array();
+    };
     $follow_link = 'participants.php';
 }
 
 $context = get_context_instance(CONTEXT_SYSTEM);
 $PAGE->set_context($context);
 
+$PAGE->navbar->add($blockname);
 $PAGE->navbar->add($header);
 $PAGE->set_title($blockname . ': ' .$header);
 $PAGE->set_heading($SITE->shortname. ": " . $blockname);
 $PAGE->set_url('/blocks/helpdesk/', array('mode' => $mode));
-
-echo $OUTPUT->header();
-echo $OUTPUT->heading($header);
 
 $availability = array(
     'contains' => get_string('contains', 'block_helpdesk'),
@@ -52,23 +55,28 @@ $availability = array(
     'ends' => get_string('ends', 'block_helpdesk')
 );
 
-$template_data = array(
+$form = new helpdesk_searchform(null, array(
     'availability' => $availability,
     'criterion' => $criterion,
-    'mode' => $mode,
-    'data' => $data,
-    'results' => $results,
-    'follow_link' => $follow_link,
-);
+    'mode' => $mode
+));
 
-$registers = array(
-    'function' => array(
-        'fullname' => function($params, &$smarty) {
-            extract($params);
-            return !empty($obj->fullname) ? $obj->fullname : fullname($obj);
-        },
-        'helplinks' => function($params, &$smarty) use ($mode, $context) {
-            extract($params);
+if ($form->is_cancelled()) {
+    redirect(new moodle_url('/my'));
+} else if ($data = $form->get_data()) {
+    $return = $results($data, $count);
+
+    if ($count) {
+        $table = new html_table();
+        $table->head = array(get_string('fullname'));
+        if ($mode == 'user') {
+            $table->head[] = get_string('idnumber');
+        }
+        $table->head[] = get_string('action');
+
+        foreach ($results($data, $count) as $obj) {
+            $fn = !empty($obj->fullname) ? $obj->fullname : fullname($obj);
+
             $help = new stdClass;
             $help->{$mode . 'id'} = $obj->id;
             $help->links = array();
@@ -76,12 +84,36 @@ $registers = array(
             $help->$mode = $obj;
 
             events_trigger('helpdesk_' . $mode, $help);
-            return implode (' | ', $help->links);
-        }
-    )
-);
 
-quick_template::render("helpdesk_index.tpl", $template_data, 'block_helpdesk', $registers);
+            $url = new moodle_url($follow_link, array('id' => $obj->id));
+
+            $attrs = (isset($obj->visible) && !$obj->visible) ?
+                array('class' => 'dimmed') : array();
+
+            $line = array(html_writer::link($url, $fn, $attrs));
+            if ($mode == 'user') {
+                $line[] = $obj->idnumber;
+            }
+            $line[] = implode(' | ', $help->links);
+
+            $table->data[] = new html_table_row($line);
+        }
+    }
+}
+
+echo $OUTPUT->header();
+echo $OUTPUT->heading($header);
+
+$form->display();
+
+if ($data) {
+    if (empty($count)) {
+        $OUTPUT->notification(get_string('no_results', 'block_helpdesk'));
+    } else {
+        echo html_writer::tag('div',
+            html_writer::table($table),
+            array('class' => 'box'));
+    }
+}
 
 echo $OUTPUT->footer();
-
---
+++ searchform.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once $CFG->libdir . '/formslib.php';
+
+class helpdesk_searchform extends moodleform {
+    function definition() {
+        $m =& $this->_form;
+
+        foreach ($this->_customdata['criterion'] as $k => $c) {
+            $options = $this->_customdata['availability'];
+            $elements = array(
+                $m->createElement('select', "{$k}_equality", '', $options),
+                $m->createElement('text', "{$k}_terms", '', array('size' => 60))
+            );
+
+            $m->addGroup($elements, $k, $c, array(' '), false);
+            $m->setDefault("{$k}_equality", 'contains');
+        }
+
+        $m->addElement('hidden', 'mode', $this->_customdata['mode']);
+
+        $buttons = array(
+            $m->createElement('submit', 'submit', get_string('submit')),
+            $m->createElement('cancel')
+        );
+
+        $m->addGroup($buttons, 'action_buttons', "&nbsp;", array(' '), false);
+    }
+}
