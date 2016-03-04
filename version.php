cfb97200542a176b9651370ba36cc43db332577f
Chad Mazilly <chadmazilly@gmail.com>
2016-03-04 09:53:40 -0600

    bump version number, add ues 3.0 as a dependency

--- version.php
+++ version.php
@@ -1,35 +1,18 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 
 /**
  * @package   block_helpdesk
- * @copyright 2014, Louisiana State University
+ * @copyright 2016, Louisiana State University
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
- 
+
 defined('MOODLE_INTERNAL') || die();
 
-
-$plugin->version = 2014060500;
-$plugin->requires = 2013051400;
 $plugin->component = 'block_helpdesk';
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.1.3';
+$plugin->version = 2016022912;
+$plugin->requires = 2015111600;
+$plugin->release = 'v3.0.0';
 
 $plugin->dependencies = array(
-    'enrol_ues' => 2013081007,
+    'enrol_ues' => 2016022912,
 );
